4b6575525fd9f4f89690385527e4e454d2f6ce96
imuhammadshoaib <msr-shoaib@hotmail.com>
2020-07-26 17:06:52 +0500

    Boilerplate

---
+++ includes/class-ms-taxamo-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.muhammadshoaib.me
+ * @since      1.0.0
+ *
+ * @package    Ms_Taxamo
+ * @subpackage Ms_Taxamo/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Ms_Taxamo
+ * @subpackage Ms_Taxamo/includes
+ * @author     Muhammad Shoaib <hello@muhammadshoaib.me>
+ */
+class Ms_Taxamo_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
